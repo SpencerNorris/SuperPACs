6c80e8dd6ad4e5e766c3e9dad6d523161167fbad
Saswat Bhattacharya <sas.saswat@gmail.com>
2017-03-06 18:41:06 -0500

    Fixed some webpack warnings

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -192,11 +192,7 @@ module.exports = function makeWebpackConfig() {
     config.plugins.push(
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
       // Only emit files when there are no errors
-      new webpack.NoErrorsPlugin(),
-
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-      // Dedupe modules in the output
-      new webpack.optimize.DedupePlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
 
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
