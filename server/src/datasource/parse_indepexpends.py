e2cbc0a3acf793ca8c45eb17cb0071a254a7e2b7
reitakCS <reitak@rpi.edu>
2017-03-29 13:49:29 -0400

    Update parse_indepexpends.py
    Updated script to parse for donations to congressmen only

--- server/src/datasource/parse_indepexpends.py
+++ server/src/datasource/parse_indepexpends.py
@@ -9,9 +9,21 @@ ProPublica_APIKEY = os.getenv('PP_API_KEY', '')
 FecApiObj = fec.FECAPI(FEC_APIKEY)
 committees = FecApiObj.get_committees()
 PPCampFinObj = propublica.CampaignFinanceAPI(ProPublica_APIKEY)
-datafile = open("IndepExpends.json", 'w')
+PPCongressApi = propublica.CongressAPI(ProPublica_APIKEY)
+legislator_index = list()
+legislators = PPCongressApi.list_members('house')["results"][0]["members"]
+for legislator in legislators:
+    name = str(legislator['first_name']) + " " + str(legislator['last_name'])
+    legislator_index.append(name)
+legislators = PPCongressApi.list_members('senate')["results"][0]["members"]
+for legislator in legislators:
+    name = str(legislator['first_name']) + " " + str(legislator['last_name'])
+    legislator_index.append(name)
+
+
 for committee in committees:
     if(2016 in committee['cycles']):
         indepExpend = PPCampFinObj.get_indep_expends(str(committee['committee_id']))
-        datafile.write(str(indepExpend))
-datafile.close()
+        for expend in indepExpend["results"]:
+            if(expend['candidate_name'] in legislator_index):
+                #expend fo a particular expenditure
