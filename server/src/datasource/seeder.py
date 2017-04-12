5f7e98ec272803531ddf8b9c8b13699015812427
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2017-04-12 14:55:38 -0400

    Added seeder abstract base class

--- server/src/datasource/seeder.py
+++ server/src/datasource/seeder.py
@@ -19,8 +19,8 @@ from parse_indepexpends import *
 
 ProPublica_APIKEY = os.getenv('PP_API_KEY', '')
 FEC_APIKEY = os.getenv('FEC_API_KEY', '')
-
-class Seeder:
+##abstract base class for
+class AbstractSeeder:
     ##api keys
     ProPublica_APIKEY = ""
     FEC_API_KEY = ""
@@ -28,68 +28,116 @@ class Seeder:
     ##Django environment
     os.environ['DJANGO_SETTINGS_MODULE'] = 'rest.settings'
 
+    ##Find out way to pass in the testing or production database to seed.
+    ##Unfortunately, it is currently based on the environment where this seeder object is called.
 
-def uploadRepresentatives():
-    ##get all the representatives in json
-    con_obj = CongressAPI(apikey=ProPublica_APIKEY,congressnum=115)
-    congressmen_list = con_obj.list_members(chamber = "house")
-    senators_list = con_obj.list_members(chamber = "senate")
+    ##pickled, cached data
+    donation_data_file="donationdata.pickle"
 
-    #print(congressmen_list['results'][0]['members'][0])
-    for congressman in congressmen_list['results'][0]['members']:
-        congress_dict = {} #personal details
-        congress_dict["propublicaid"] = congressman['id']
-        congress_dict["first_name"] = congressman['first_name']
-        congress_dict["last_name"] = congressman['last_name']
-        #office details
-        congress_dict["district"] = congressman['district']
-        congress_dict["state"] = congressman['state']
-        congress_dict["party"] = congressman['party']
-        congress_dict["chamber"] = "H"
+    def getRepresentatives():
+        pass
+    def uploadRepresentatives(representatives_list):
+        pass
 
-        Representative.objects.create(**congress_dict)
+    def getSuperPACs():
+        pass
+    def uploadSuperPACs(superpacs_list):
+        pass
 
-    for senator in senators_list['results'][0]['members']:
-        senator_dict = {}#personal details
-        senator_dict["propublicaid"] = senator['id']
-        senator_dict["first_name"] = senator['first_name']
-        senator_dict["last_name"] = senator['last_name']
-        #office details
-        senator_dict["state"] = senator['state']
-        senator_dict["party"] = senator['party']
-        senator_dict["chamber"] = "S"
+    def getDonations():
+        pass
+    def uploadDonations(donations_list):
+        pass
 
-        Representative.objects.create(**senator_dict)
+class Seeder(AbstractSeeder):
+    ##api keys
+    ProPublica_APIKEY = ""
+    FEC_API_KEY = ""
 
-    return True
+    ##Django environment
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'rest.settings'
 
-def uploadSuperPACs():
-    fec_obj = FECAPI(FEC_APIKEY)
-    superpacs_list = fec_obj.get_committees()
+    ##Find out way to pass in the testing or production database to seed.
+    ##Unfortunately, it is currently based on the environment where this seeder object is called.
 
-    #print(superpacs_list[0])
-    for superpac in superpacs_list:
-        superpac_dict = {}
-        superpac_dict["name"]=superpac["name"]
-        superpac_dict["fecid"]=superpac["committee_id"]
-        SuperPAC.objects.create(**superpac_dict)
+    ##pickled, cached data
+    donation_data_file="donationdata.pickle"
 
-def uploadDonations(filename='donationdata.pickle'):
-    donation_list = donations(filename)
-    print(donation_list[0])
-    for donation in donation_list:
-        donation_dict = {}
+    def getRepresentatives():
+        ##get all the representatives in json
+        con_obj = CongressAPI(apikey=ProPublica_APIKEY,congressnum=115)
+        house_list = con_obj.list_members(chamber = "house")
+        senators_list = con_obj.list_members(chamber = "senate")
 
-        rep = Representative.objects.get(propublicaid=donation["propublica_candidate_id"])
-        sup = SuperPAC.objects.get(fecid=donation["committee_id"])
+        congress_list = {}
+        congress_list["house"] = house_list
+        congress_list["senate"] = senate_list
+        return congress_list
 
-        donation_dict["representative_id"] = rep.id
-        donation_dict["superpac_id"] = sup.id
-        donation_dict["amount"] = donation["amount"]
-        donation_dict["uid"] = donation["unique_id"]
-        donation_dict["support"] = donation["support_or_oppose"]
-        Donation.objects.create(**donation_dict)
-        #print("get donation")
+    def uploadRepresentatives(congress_list):
+        for congressman in congress_list["house"]['results'][0]['members']:
+            congress_dict = {} #personal details
+            congress_dict["propublicaid"] = congressman['id']
+            congress_dict["first_name"] = congressman['first_name']
+            congress_dict["last_name"] = congressman['last_name']
+            #office details
+            congress_dict["district"] = congressman['district']
+            congress_dict["state"] = congressman['state']
+            congress_dict["party"] = congressman['party']
+            congress_dict["chamber"] = "H"
+
+            Representative.objects.create(**congress_dict)
+
+        for senator in congress_list["senate"]['results'][0]['members']:
+            senator_dict = {}#personal details
+            senator_dict["propublicaid"] = senator['id']
+            senator_dict["first_name"] = senator['first_name']
+            senator_dict["last_name"] = senator['last_name']
+            #office details
+            senator_dict["state"] = senator['state']
+            senator_dict["party"] = senator['party']
+            senator_dict["chamber"] = "S"
+
+            Representative.objects.create(**senator_dict)
+
+    def getSuperPACs():
+        fec_obj = FECAPI(FEC_APIKEY)
+        superpacs_list = fec_obj.get_committees()
+        return superpacs_list
+
+    def uploadSuperPACs(superpac_list):
+        for superpac in superpacs_list:
+            superpac_dict = {}
+            superpac_dict["name"]=superpac["name"]
+            superpac_dict["fecid"]=superpac["committee_id"]
+            SuperPAC.objects.create(**superpac_dict)
+
+    def getDonations():
+
+        return donation_list
+
+    def getCachedDonations():
+        donation_list = donations(filename)
+        return donation_list
+
+    def getAPIDonations():
+        donation_list = donations(filename)
+        return donation_list
+
+    def uploadDonations(donation_list):
+        for donation in donation_list:
+            donation_dict = {}
+
+            rep = Representative.objects.get(propublicaid=donation["propublica_candidate_id"])
+            sup = SuperPAC.objects.get(fecid=donation["committee_id"])
+
+            donation_dict["representative_id"] = rep.id
+            donation_dict["superpac_id"] = sup.id
+            donation_dict["amount"] = donation["amount"]
+            donation_dict["uid"] = donation["unique_id"]
+            donation_dict["support"] = donation["support_or_oppose"]
+            Donation.objects.create(**donation_dict)
+
 
 def uploadToDatabase(picklefilename):
     Donation.objects.all().delete()
