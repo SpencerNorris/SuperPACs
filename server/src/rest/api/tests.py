440a7c3b88e97c12ee6ec836938ccd11c56aacce
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2017-04-13 19:45:40 -0400

    Improved the quality of the test file

--- server/src/rest/api/tests.py
+++ server/src/rest/api/tests.py
@@ -8,26 +8,44 @@ srcpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.append("/code/src/datasource")
 print(os.sys.path)
 from seeder import *
-from api.models import Representative
-# Create your tests here.
+from api.models import *
 
+from api.views import *
+# Create your tests here.
 
 #this file is for
 
+##Class API to Pickle simple comparator.
+##This testing class would test that all the data
+## that is coming from the API's directly would get saved to pickles when required.
+##and that the data for those pickles matches the same API data after it gets saved.
 
+#What about API to pickle? or is that way too deep.
 class CacheToORMTest(TestCase):
     def setUp(self):
-        uploadToDatabase()
+        self.api_seeder = APISeeder()
 
-        print("number of representatives:",len(Representative.objects.all()))
+        print("(start)CacheToORMTest(setUp):--------")
+        self.pickle_seeder = PickleSeeder()
+        print("Using PickleSeeder seeder to get data.")
+        self.pickle_seeder.seedAll(reset=False)
+        print("Finished using PickleSeeder.")
+
+
+        #print("Number of representatives:",len(Representative.objects.all()))
+        print("Getting ORM values to compare with API json.")
         self.ted_cruz = Representative.objects.get(first_name="Ted",last_name="Cruz")
         self.elizabeth_warren = Representative.objects.get(first_name="Elizabeth",last_name="Warren")
+        print("(end)CacheToORMTest(setUp):--------")
 
 
     def tearDown(self):
         pass
 
     def test_partyperson(self):
+        ##compare the data you can get from the Seeder to data in the original JSON
+        ##essentially, create content based comparisons like this:
+        ##self.api_seeder.getRepresentatives()["Ted_cruz"]["party"] == self.ted_cruz.party
         self.assertEqual(self.ted_cruz.party, 'R')
         self.assertEqual(self.elizabeth_warren.party, 'D')
         pass
@@ -36,11 +54,26 @@ class CacheToORMTest(TestCase):
 #import django views to sanity check the front-end visualization contract.
 class ViewsToFrontendTest(TestCase):
     def setUp(self):
-        uploadToDatabase()
-        print("Put everything in test database.")
+
+        self.api_seeder = APISeeder()
+
+        print("(start)ViewsToFrontendTest(setUp):--------")
+        self.pickle_seeder = PickleSeeder()
+        print("Using PickleSeeder seeder to get data.")
+        self.pickle_seeder.seedAll(reset=False)
+
+
+        print("Getting ORM values to compare with VIEW json.")
+        self.ted_cruz = Representative.objects.get(first_name="Ted",last_name="Cruz")
+        self.elizabeth_warren = Representative.objects.get(first_name="Elizabeth",last_name="Warren")
+        print("(end)ViewsToFrontendTest(setUp):--------")
 
     def tearDown(self):
         pass
 
     def test_partyperson(self):
+        ##compare the data you can get from the ORM to data in the view
+        ##essentially, create content based comparisons like this:
+        ##self.ted_cruz.party == views.donations()["ted_cruz"].party
+
         pass
