5d03f32209ccbb27f0eba45f3ddd340446434ffe
spencer <spencercnorris2017@gmail.com>
2017-04-18 22:33:23 -0400

    expanded sunlight.py with votes method

--- server/src/datasource/sunlight.py
+++ server/src/datasource/sunlight.py
@@ -1,7 +1,3 @@
-from datasource import RESTDatasource
-from datasource import APIKeyException
-from datasource import URIParameterException
-
 class SunlightAPI(RESTDatasource):
 	def __init__(self):
 		apikey = '' #Sunlight doesn't require one!
@@ -30,6 +26,7 @@ class SunlightAPI(RESTDatasource):
 		endpoint = 'https://congress.api.sunlightfoundation.com/bills/search'
 		return super().request(endpoint, params, headers)
 
+
 	def bills(self, **params):
 	'''
 	The main endpoint of Sunlight for retrieving data about legislation.
@@ -44,4 +41,43 @@ class SunlightAPI(RESTDatasource):
 	}
 	'''
 		endpoint = 'https://congress.api.sunlightfoundation.com/bills'
+		return super().request(endpoint, params, headers)
+
+
+	def votes(self, **params):
+	'''
+	The main endpoint of Sunlight for retrieving data about votes.
+	Various parameters can be passed to retrieve votes according to certain criteria.
+
+	For a list of fields which can be used for filtering, see:
+	https://sunlightlabs.github.io/congress/bills.html .
+
+		Append the desired fields as keys in **params, such as:
+	{
+		'enacted_as.law_type' : 'private'
+	}
+
+	Most relevant to us is how to retrieve votes related to a particular bill.
+	In order to do this, append the bill_id to **params. For example:
+	{
+		"bill_id": "hr41-113"
+	}
+
+	The following is an example return value:
+	{
+	  "roll_id": "h7-2013",
+	  "chamber": "house",
+	  "number": 7,
+	  "year": 2013,
+	  "congress": 113,
+	  "voted_at": "2013-01-04T16:22:00Z",
+	  "vote_type": "passage",
+	  "roll_type": "On Motion to Suspend the Rules and Pass",
+	  "question": "On Motion to Suspend the Rules and Pass -- H.R. 41 -- To temporarily increase the borrowing authority of the Federal Emergency Management Agency for carrying out the National Flood Insurance Program",
+	  "required": "2/3",
+	  "result": "Passed",
+	  "source": "http://clerk.house.gov/evs/2013/roll007.xml"
+	}
+	'''
+		endpoint = 'https://congress.api.sunlightfoundation.com/votes'
 		return super().request(endpoint, params, headers)
