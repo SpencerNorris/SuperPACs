dd7bf6f07afdaa03cbd93cedea5d96aef45864ff
spencer <spencercnorris2017@gmail.com>
2017-04-19 13:30:00 -0400

    added sunlight.py, expanded propublica.py with vote method

--- server/src/datasource/sunlight.py
+++ server/src/datasource/sunlight.py
@@ -4,80 +4,87 @@ class SunlightAPI(RESTDatasource):
 		super().__init__(apikey)
 
 
-	def search(self, query, **params):
-	'''
-	From the Sunlight API documentation:
+	def search(self, query="obamacare"):
+		'''
+		From the Sunlight API documentation:
 
-	Search the full text of legislation, and other fields.
-	The query parameter allows wildcards, quoting for phrases, and nearby
-	word operators (full reference) You can also retrieve highlighted excerpts,
-	and all normal operators and filters.
-	This searches the bill’s full text, short_title, official_title, popular_title, 
-	nicknames, summary, and keywords fields.
+		Search the full text of legislation, and other fields.
+		The query parameter allows wildcards, quoting for phrases, and nearby
+		word operators (full reference) You can also retrieve highlighted excerpts,
+		and all normal operators and filters.
+		This searches the bill’s full text, short_title, official_title, popular_title, 
+		nicknames, summary, and keywords fields.
 
-	For a list of fields which can be used for filtering, see:
-	https://sunlightlabs.github.io/congress/bills.html .
+		For a list of fields which can be used for filtering, see:
+		https://sunlightlabs.github.io/congress/bills.html .
 
-	Append the desired fields as keys in **params, such as:
-	{
-		'enacted_as.law_type' : 'private'
-	}
-	'''
+		Append the desired fields as keys in **params, such as:
+		{
+			'enacted_as.law_type' : 'private'
+		}
+		'''
 		endpoint = 'https://congress.api.sunlightfoundation.com/bills/search'
-		return super().request(endpoint, params, headers)
+		params = {}
+		params['query'] = query
+		return super().request(endpoint, params, {})
 
 
-	def bills(self, **params):
-	'''
-	The main endpoint of Sunlight for retrieving data about legislation.
-	Various parameters can be passed to retrieve bills according to certain criteria.
+	def bills(self, bill_id, congress=115):
+		'''
+		The main endpoint of Sunlight for retrieving data about legislation.
+		Various parameters can be passed to retrieve bills according to certain criteria.
 
-	For a list of fields which can be used for filtering, see:
-	https://sunlightlabs.github.io/congress/bills.html .
+		For a list of fields which can be used for filtering, see:
+		https://sunlightlabs.github.io/congress/bills.html .
 
-		Append the desired fields as keys in **params, such as:
-	{
-		'enacted_as.law_type' : 'private'
-	}
-	'''
+			Append the desired fields as keys in **params, such as:
+		{
+			'enacted_as.law_type' : 'private'
+		}
+		'''
+		params = {}
+		params['bill_id'] = bill_id
+		params['congress'] = congress
 		endpoint = 'https://congress.api.sunlightfoundation.com/bills'
-		return super().request(endpoint, params, headers)
+		return super().request(endpoint, params, {})
 
 
-	def votes(self, **params):
-	'''
-	The main endpoint of Sunlight for retrieving data about votes.
-	Various parameters can be passed to retrieve votes according to certain criteria.
+	def votes(self, bill_id):
+		'''
+		The main endpoint of Sunlight for retrieving data about votes.
+		Various parameters can be passed to retrieve votes according to certain criteria.
 
-	For a list of fields which can be used for filtering, see:
-	https://sunlightlabs.github.io/congress/bills.html .
+		For a list of fields which can be used for filtering, see:
+		https://sunlightlabs.github.io/congress/bills.html .
 
-		Append the desired fields as keys in **params, such as:
-	{
-		'enacted_as.law_type' : 'private'
-	}
+			Append the desired fields as keys in **params, such as:
+		{
+			'enacted_as.law_type' : 'private'
+		}
 
-	Most relevant to us is how to retrieve votes related to a particular bill.
-	In order to do this, append the bill_id to **params. For example:
-	{
-		"bill_id": "hr41-113"
-	}
+		Most relevant to us is how to retrieve votes related to a particular bill.
+		In order to do this, append the bill_id to **params. For example:
+		{
+			"bill_id": "hr41-113"
+		}
 
-	The following is an example return value:
-	{
-	  "roll_id": "h7-2013",
-	  "chamber": "house",
-	  "number": 7,
-	  "year": 2013,
-	  "congress": 113,
-	  "voted_at": "2013-01-04T16:22:00Z",
-	  "vote_type": "passage",
-	  "roll_type": "On Motion to Suspend the Rules and Pass",
-	  "question": "On Motion to Suspend the Rules and Pass -- H.R. 41 -- To temporarily increase the borrowing authority of the Federal Emergency Management Agency for carrying out the National Flood Insurance Program",
-	  "required": "2/3",
-	  "result": "Passed",
-	  "source": "http://clerk.house.gov/evs/2013/roll007.xml"
-	}
-	'''
+		The following is an example return value:
+		{
+		  "roll_id": "h7-2013",
+		  "chamber": "house",
+		  "number": 7,
+		  "year": 2013,
+		  "congress": 113,
+		  "voted_at": "2013-01-04T16:22:00Z",
+		  "vote_type": "passage",
+		  "roll_type": "On Motion to Suspend the Rules and Pass",
+		  "question": "On Motion to Suspend the Rules and Pass -- H.R. 41 -- To temporarily increase the borrowing authority of the Federal Emergency Management Agency for carrying out the National Flood Insurance Program",
+		  "required": "2/3",
+		  "result": "Passed",
+		  "source": "http://clerk.house.gov/evs/2013/roll007.xml"
+		}
+		'''
 		endpoint = 'https://congress.api.sunlightfoundation.com/votes'
-		return super().request(endpoint, params, headers)
+		params = {}
+		params['bill_id'] = bill_id
+		return super().request(endpoint, params, {})
