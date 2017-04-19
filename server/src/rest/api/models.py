55bbd7de42887abc9c7285426593ab0c1570bc41
Kevin <reitak@rpi.edu>
2017-04-19 14:12:23 -0400

    Remove bill/vote stuff from models

--- server/src/rest/api/models.py
+++ server/src/rest/api/models.py
@@ -64,65 +64,6 @@ class SuperPAC(models.Model):
     def __json__(self):
         return {"id":self.id,"name":self.name}
 
-
-class Bill(models.Model):
-    '''
-    Bill class which represents Bill. It is either passed or not passed.
-    '''
-    bill_id = models.charField(max_length=12)
-    display_title = models.CharField(max_length = 140)
-    official_title = models.CharField(max_length = 140)
-    source = models.CharField(max_length = 500,default="")
-    congress = models.IntegerField()
-    subjects = models.ManyToManyField(BillSubject)
-
-    CHAMBERS = (
-                ("H","House"),
-                ("S","Senate"),
-            )
-    chamber = models.CharField(max_length=1, choices=CHAMBERS)
-
-
-    BILL_TYPES = (
-                  ("hr", "House Bill"),
-                  ("s", "Senate Bill"),
-                  ("hres", "House Resolution"),
-                  ("sres", "Senate Resolution"),
-                  ("hconres", "House Concurrent Resolution"),
-                  ("sconres", "Senate Concurrent Resolution"),
-                  ("hjres", "House Joint Resolution"),
-                  ("sjres", "Senate Joint Resolution"),
-                )
-    bill_type = models.CharField(max_length=7, choices=BILL_TYPES)
-
-
-    ##PROPUBLICA api id.
-    propublicaid = models.CharField(max_length = 9,default = None)
-
-    ##FEC api id
-    fecid = models.CharField(max_length = 9,default = None)
-
-    def __str__(self):
-        return self.display_title
-
-
-class BillSubject(models.Model):
-    subject = models.CharField(max_length="100")
-    def __str__(self):
-        return subject
-
-class Vote(models.Model):
-    '''
-    Vote class which represents a Representative voting on a Bill.
-    '''
-    representative = models.ForeignKey(Representative, on_delete=models.CASCADE)
-    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
-    decision = models.IntegerField()
-
-    def __str__(self):
-        return self.representative.__str__()+","+self.bill.__str__()+","+self.decision
-
-
 class Donation(models.Model):
     '''
     Donation class that acts as a donation edge from SuperPACs to Representatives.
